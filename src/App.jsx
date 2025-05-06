ad9d1aa6c4101ce7495998b847651c6b192f6b22
kruti-patel2815 <krutipatel2811.cdmi@gmail.com>
2025-05-06 17:46:09 +0530

    props task

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Second from './Second';
 import Third from './Third';
+import PropsExample from './PropsExample';
 
 function App() {
   return (
     <>
-    <Second>
+     <Second>
 
     </Second>
     <Third>
       
     </Third>
+    <PropsExample demo="hello"></PropsExample> 
+    <PropsExample demo="hello"></PropsExample> 
+     <PropsExample demo={{name : "demo" , age : 12}}></PropsExample> 
+    <PropsExample demo={[{name : "demo" , age : 12} , {name : "demo12" , age : 1212}]}></PropsExample>
     </>
   );
 }
---
+++ src/PropsExample.jsx
@@ -0,0 +1,82 @@
+// import React from 'react'
+
+// const PropsExample = (props) => {
+//   return (
+//     <div>
+//         <h1>{props.demo}</h1>
+//     </div>
+//   )
+// }
+
+// export default PropsExample
+
+
+
+// import React from 'react'
+
+// const PropsExample = ({demo}) => {
+//   return (
+//     <div>
+//         <h1>{demo}</h1>
+//     </div>
+//   )
+// }
+
+// export default PropsExample
+
+
+// import React from 'react'
+
+// const PropsExample = ({demo}) => {
+//   return (
+//     <div>
+//         <h1>{demo.name}</h1>
+//         <h1>{demo.age}</h1>
+//     </div>
+//   )
+// }
+
+// export default PropsExample
+
+
+import React from 'react'
+
+const PropsExample = ({demo}) => {
+  return (
+    <div>
+        {/* <h1>{demo[0].name}</h1> */}
+        {/* <h1>{demo[0].age}</h1> */}
+
+        <table border={1}>
+            <tr>
+                <td>name</td>
+                <td>age</td>
+            </tr>
+            {/* {
+                demo.map((item , index) => {
+                    return (
+                        <tr>
+                            <td>{item.name}</td>
+                            <td>{item.age}</td>
+                        </tr>
+                    )
+                })
+            } */}
+
+{
+                demo.map((item , index) => (
+                    
+                        <tr>
+                            <td>{item.name}</td>
+                            <td>{item.age}</td>
+                        </tr>
+                    )
+                )
+            }
+        </table>
+
+    </div>
+  )
+}
+
+export default PropsExample
